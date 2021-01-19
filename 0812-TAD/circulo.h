a92a94f6292f0cefd6fabb54155e043702b997a6
Klayver Ximenes <klayverx@gmail.com>
2021-01-18 22:59:33 -0300

    att aula

--- 0812-TAD/circulo.h
+++ 0812-TAD/circulo.h
@@ -2,17 +2,17 @@
 
 typedef struct circulo Circulo;
 
-/*Função que cria e retorna um círculo com centro (x,y) e
+/*Funï¿½ï¿½o que cria e retorna um cï¿½rculo com centro (x,y) e
 raio r */
 Circulo* circ_cria (float x, float y, float r);
 
-/* Função que libera a memória de um círculo previamente
+/* Funï¿½ï¿½o que libera a memï¿½ria de um cï¿½rculo previamente
 criado */
 void circ_libera (Circulo* c);
 
-/* Função que calcula o valor da área do círculo */
+/* Funï¿½ï¿½o que calcula o valor da ï¿½rea do cï¿½rculo */
 float circ_area (Circulo* c);
 
-/* Função que verifica se um dado ponto p está dentro do
-círculo */
+/* Funï¿½ï¿½o que verifica se um dado ponto p estï¿½ dentro do
+cï¿½rculo */
 int circ_interior (Circulo* c, Ponto* p);
